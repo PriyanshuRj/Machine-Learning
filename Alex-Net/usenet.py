2f24d5f661d8a08a65b5bb406d376435e24482b7
PriyanshuRj <priyanshurajput0071109@gmail.com>
2022-03-16 07:40:30 +0530

    Changed classes to 1000

--- Alex-Net/usenet.py
+++ Alex-Net/usenet.py
@@ -4,5 +4,5 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from alexnet import AlexNet
 
 if __name__ == "__main__":
-    model = AlexNet(input_shape = (224, 224, 3), classes = 100)
+    model = AlexNet(input_shape = (224, 224, 3), classes = 1000)
     model.summary()
