ee2753bc8bbcfc331c2d49d51e08c7348ffb8002
PriyanshuRj <priyanshurajput0071109@gmail.com>
2022-05-08 14:59:19 +0530

    Updated Loss function

--- PyTorch/YOLOv3/loss.py
+++ PyTorch/YOLOv3/loss.py
@@ -50,12 +50,12 @@ class YoloLoss(nn.Module):
             (predictions[..., 5:][obj]), (target[..., 5][obj].long()),
         )
 
-        #print("__________________________________")
-        #print(self.lambda_box * box_loss)
-        #print(self.lambda_obj * object_loss)
-        #print(self.lambda_noobj * no_object_loss)
-        #print(self.lambda_class * class_loss)
-        #print("\n")
+        print("__________________________________")
+        print(self.lambda_box * box_loss)
+        print(self.lambda_obj * object_loss)
+        print(self.lambda_noobj * no_object_loss)
+        print(self.lambda_class * class_loss)
+        print("\n")
 
         return (
             self.lambda_box * box_loss
